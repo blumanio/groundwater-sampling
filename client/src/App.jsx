b9d5364f7502043d7eee568c5b16e4be826677a6
blumanio <elaammarim@gmail.com>
2026-04-12 22:11:38 +0200

    Revert "Fix logout: restore Login import and correct token guard in App.jsx"
    This reverts commit 58541079d4521813d19b1e28f6ebb9498ec415a2.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import Login from './components/Login';
 import MainApp from './MainApp'; // Import the new MainApp component
 
 function App() {
     const token = localStorage.getItem('token');
 
-    if (!token) {
-        return <Login />;
+    // If there is no login token, show the Login page
+    if (token) {
+        // return <Login />;
     }
 
+    // If a token exists, show the main application
     return <MainApp />;
 }
 
